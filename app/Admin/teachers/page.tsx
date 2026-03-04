4cfdd8d0cdb87ce371b67bdbccd184eee8cbe032
Fatima <bc220417002fzn@vu.edu.pk>
2026-03-05 02:58:34 +0500

    changes in leave status

--- app/Admin/teachers/page.tsx
+++ app/Admin/teachers/page.tsx
@@ -104,7 +104,7 @@ const TeachersPage = () => {
             teacher.status === "active"
               ? "bg-green-600 text-white/800"
               : teacher.status === "on-leave"
-              ? "bg-yellow-500/20 text-white/80"
+              ? "bg-yellow-600 text-white/80"
               : "bg-gray-500/20 ttext-white/80"
           }`}
         >
